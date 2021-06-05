a2a90aa469b710493a2852b9f5b56fc61953ca4a
Stephan Dilly <dilly.stephan@gmail.com>
2021-06-05 10:47:02 +0200

    fix nightly clippy

--- Makefile
+++ Makefile
@@ -2,10 +2,8 @@
 check:
 	cargo c
 	cargo fmt -- --check
-	cargo clean -p fb-api
 	cargo clippy
 	cargo t
 
-clippy-nightly:
-	cargo +nightly clean -p fb-api
+check-nightly:
 	cargo +nightly clippy
--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,22 @@
-#![allow(clippy::pub_enum_variant_names)]
-
 use std::string::FromUtf8Error;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("parse error:{0}")]
-    UrlParseError(#[from] url::ParseError),
+    UrlParse(#[from] url::ParseError),
 
     #[error("utf8 error:{0}")]
-    Utf8Error(#[from] FromUtf8Error),
+    Utf8Parse(#[from] FromUtf8Error),
 
     #[error("hyper error:{0}")]
-    HyperError(#[from] hyper::Error),
+    Hyper(#[from] hyper::Error),
 
     #[error("http error:{0}")]
-    HttpError(#[from] hyper::http::Error),
+    Http(#[from] hyper::http::Error),
 
     #[error("json error:{0}")]
-    JsonParseError(#[from] serde_json::Error),
+    JsonParse(#[from] serde_json::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
